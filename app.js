2cb3e43b374f9eb717b36d9093731e9df5741a2d
Seth DImmer <sethdimmer1@gmail.com>
2022-07-01 16:45:48 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
 // console.log('hello world '+'4234##&*798789')
-console.log('Hello World'[3]) /*To select the index that i want put either 0,1,2,3,4  */
+console.log('Hello World'[2]) /*To select the index that i want put either 0,1,2,3,4  */
