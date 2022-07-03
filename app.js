c8e7fb843a9f955490850f86e1b8d985af8f93bf
Seth DImmer <sethdimmer1@gmail.com>
2022-07-02 22:55:45 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 // console.log('hello world '+'4234##&*798789')     /*The space between the words "Hello" and "World" count as a string index and it currently the 5th string */
 console.log('Hello World'[10]) /*To select the index that i want put either 0,1,2,3,4  */
-console.log('12'.length)
+console.log('123'.length)
