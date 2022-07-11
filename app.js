a02109656c1c39876728590c3f954b4b84dc041b
Seth DImmer <sethdimmer1@gmail.com>
2022-07-11 13:29:09 -0400

    initial commit

--- app.js
+++ app.js
@@ -140,6 +140,23 @@ let loggedIn = true
     // console.log("not enough money - you still owe x dollars")
 // }
 
+
+/*43:21 */
+// let cash = 100
+// let price = 40
+
+// if(cash > price) {    
+    // console.log(`you paid extra - here's ${cash - price} change`)
+// }
+// else if (cash === price) {
+    // console.log("you paid the exact amount, have a nice day!")
+// }
+// else {
+    // console.log(`not enough money - you still owe ${cash - price} dollars`)
+// }
+
+/* 43:38 */
+
 let cash = 45
 let price = 40
 
