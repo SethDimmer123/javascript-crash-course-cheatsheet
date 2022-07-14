cac605a9ea8c34655a6f6864f6a48af74564f5ab
Seth DImmer <sethdimmer1@gmail.com>
2022-07-14 15:35:43 -0400

    initial commit

--- app.js
+++ app.js
@@ -428,7 +428,7 @@ let isRaining = true/*boolean*/
              * If the number is *not* divisible by either 3 or 5, print the number
              * 
              *  @example
-             * 1 -> 1
+             * 1 -> 1 ---> 1 prints one
              * 2 -> 2
              * 3 -> "Frontend"
              * 4 -> 4
