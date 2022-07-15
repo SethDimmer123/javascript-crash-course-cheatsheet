ea44dac8f80d4bbb4871fb0b06954dca5cfcf59a
Seth DImmer <sethdimmer1@gmail.com>
2022-07-15 14:50:06 -0400

    initial commit

--- app.js
+++ app.js
@@ -441,6 +441,7 @@ let isRaining = true/*boolean*/
             // Check page 2 of for-loop problem in Notes
             for(let i = 1; i <= 20; ++i) {
                 console.log(i)
+                if()
             }
 
 
