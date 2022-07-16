ecda4cd18ecf59b2147d1b9bf71b4d718348f1c9
Seth DImmer <sethdimmer1@gmail.com>
2022-07-16 09:22:40 -0400

    initial commit

--- app.js
+++ app.js
@@ -442,8 +442,13 @@ let isRaining = true/*boolean*/
             for(let i = 1; i <= 20; ++i) {
                 // console.log(i)
                 if (i % 3 === 0){
-                    console.log(`Frontend`)
+                    console.log(`${i}Frontend`)
                 }
+                if(i % 5 === 0) {
+                    console.log(`${i}Frontend`)
+                }
+                if()
+            
             }
 
 
