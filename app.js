64b723359b340f066880b27a78c2200a3e5d1c62
Seth DImmer <sethdimmer1@gmail.com>
2022-07-16 18:44:19 -0400

    initial commit

--- app.js
+++ app.js
@@ -440,9 +440,11 @@ let isRaining = true/*boolean*/
              */
             // Check page 2 of for-loop problem in Notes
             for(let i = 1; i <= 20; ++i) {
-                
+                if(i % 3 === 0 && i % 5 === 0) {
+                    console.log(`${i} -> Frontend Simplified`)
+                }
                 // console.log(i)
-                if (i % 3 === 0){
+                 if (i % 3 === 0){
                     console.log(`${i} -> Frontend`)
                 }
                 if(i % 5 === 0) {
