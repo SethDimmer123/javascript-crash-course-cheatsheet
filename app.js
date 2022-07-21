09fe66237450b059b99040b063aef965bf2bb90a
Seth DImmer <sethdimmer1@gmail.com>
2022-07-21 08:01:50 -0400

    initial commit

--- app.js
+++ app.js
@@ -631,9 +631,13 @@ let isRaining = true/*boolean*/
 
             // THIS IS WHY ARRAYS EXIST
 
-            let arr = [20,30,40,50,100,'',Boolean]
+            let arr = [20,30,40,50,100]
+            
+            // let arr = [20,30,40,50,100,'',Boolean]
 
             // You can put multiple data types in arrays like boolean or strings
+
+
 
 
 
