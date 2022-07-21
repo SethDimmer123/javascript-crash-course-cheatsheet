1a1be948bb7e7d733ba163db60de98446591d1d9
Seth DImmer <sethdimmer1@gmail.com>
2022-07-21 04:31:06 -0400

    initial commit

--- app.js
+++ app.js
@@ -580,13 +580,19 @@ let isRaining = true/*boolean*/
              * convertCelsiusToFahrenheit(30) -> 86
              */
 
+
+            // FUNCTIONS SOLUTION 1:29:48
             function convertCelsiusToFahrenheit(celsius) {
-            return celsius * 1.8 + 32
+            return fahrenheit
             }
 
             console.log(convertCelsiusToFahrenheit(0))
             console.log(convertCelsiusToFahrenheit(10))
             console.log(convertCelsiusToFahrenheit(30))
+
+            // COME BACK AND PRACTICE THIS AGAIN
+
+
 
 
 
