42e0047a2867c42ece2969111fcf8697141c10c7
Seth DImmer <sethdimmer1@gmail.com>
2022-07-22 13:57:57 -0400

    initial commit

--- app.js
+++ app.js
@@ -692,7 +692,13 @@ let isRaining = true/*boolean*/
 
             let arr = [20,30,40,50,100]
 
-            
+            let newArr = arr.filter((element) => {
+                console.log(element)
+                if(element )
+                return true;
+            })
+
+
 
 
 
