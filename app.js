bc6a68c2898a75273b81aeeeda253fb62e879a9d
Seth DImmer <sethdimmer1@gmail.com>
2022-07-24 11:52:34 -0400

    initial commit

--- app.js
+++ app.js
@@ -771,9 +771,16 @@ let isRaining = true/*boolean*/
 
             let grades = ['A+' , 'A' , 'FAIL']
 
-            // 
+            //  ARRAY FILTER SOLUTION
 
+            let goodGrades = grades.filter((element) => {
+                console.log(element)
+                if(element !== 'FAIL') {
+                    return true;
+                }
+            })
 
+            console.log(goodGrades)
 
 
              
